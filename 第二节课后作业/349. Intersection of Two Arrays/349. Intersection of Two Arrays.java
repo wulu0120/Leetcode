187637aab4b7941093d5e4274be5c65b2d1c8984
wulu <wulu0120@gmail.com>
2022-09-12 19:38:10 -0400

    add problem 349 code

---
+++ 第二节课后作业/349. Intersection of Two Arrays/349. Intersection of Two Arrays.java
@@ -0,0 +1,21 @@
+public int[] intersection(int[] nums1, int[] nums2) {
+    // 1: 通过hashset对nums1去重
+    HashSet<Integer> set1 = new HashSet<>();
+    for(int i: nums1){
+        set1.add(i);
+    }
+    
+    // 2: loop nums2的值, 出现重复的加到新hashset里
+    HashSet<Integer> sameElement = new HashSet<>();
+    for(int j: nums2){
+        if(set1.contains(j)) sameElement.add(j);
+    }
+    
+    // 3. 返回结果
+    int[] res = new int[sameElement.size()];
+    int index = 0;
+    for(int ele: sameElement){
+        res[index++] = ele;
+    }
+    return res;
+}
